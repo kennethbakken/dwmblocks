d0e347738b14db4bce0dd4dcff8d40b519890c16
Kenneth Bakken <79416591+kennethbakken@users.noreply.github.com>
2021-02-23 20:10:08 +0100

    Add files via upload

--- config.h
+++ config.h
@@ -1,34 +1,38 @@
-/* See LICENSE file for copyright and license details. */
-/* Default settings; can be overriden by command line. */
-
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-/* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"iosevka extended:size=12",
-	"JoyPixels:pixelsize=16:antialias=true:autohint=true"
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	/* {"⌨", "sb-kbselect", 0, 30}, */
+	/* {"", "cat /tmp/recordingicon 2>/dev/null",	0,	9}, */
+	/*{"",	"sb-tasks",	10,	26}, */
+	{"",	"sb-music",	0,	11},
+	/* {"",	"sb-pacpackages",	0,	8}, */
+	/*{"",	"sb-news",		0,	6}, */
+	/* {"",	"sb-price lbc \"LBRY Token\" 📚",			9000,	22}, */
+	/* {"",	"sb-price bat \"Basic Attention Token\" 🦁",	9000,	20}, */
+	/* {"",	"sb-price link \"Chainlink\" 🔗",			300,	25}, */
+	/* {"",	"sb-price xmr \"Monero\" 🔒",			9000,	24}, */
+	/* {"",	"sb-price eth Ethereum 🍸",	9000,	23}, */
+	/* {"",	"sb-price btc Bitcoin 💰",				9000,	21}, */
+	/* {"",	"sb-torrent",	20,	7}, */
+	{"",	"sb-memory",	10,	14},
+	/* {"",	"sb-cpu",		10,	18}, */
+	/* {"",	"sb-moonphase",	18000,	17}, */
+	{"",	"sb-forecast",	18000,	5},
+	/* {"",	"sb-mailbox",	180,	12}, */
+	/* {"",	"sb-nettraf",	1,	16}, */
+	{"",	"sb-volume",	0,	10},
+	{"",	"sb-battery",	5,	3},
+	{"",	"sb-clock",	60,	1},
+	{"",	"sb-internet",	5,	4},
+	{"",	"sb-reads",	0,	15},
+	/* {"",	"sb-help-icon",	0,	15}, */
 };
-static const unsigned int bgalpha = 0xe0;
-static const unsigned int fgalpha = OPAQUE;
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#221d16" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-};
-static const unsigned int alphas[SchemeLast][2] = {
-	/*		fgalpha		bgalphga	*/
-	[SchemeNorm] = { fgalpha, bgalpha },
-	[SchemeSel] = { fgalpha, bgalpha },
-	[SchemeOut] = { fgalpha, bgalpha },
-};
 
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+//Sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char *delim = "|";
 
-/*
- * Characters not considered part of a word while deleting words
- * for example: " /?\"&[]"
- */
-static const char worddelimiters[] = " ";
+// Have dwmblocks automatically recompile and run when you edit this file in
+// vim with the following line in your vimrc/init.vim:
+
+// autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
 
